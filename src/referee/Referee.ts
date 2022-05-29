c56daa33ad10a35ab9a729e307f23f587f7be9eb
Kuchareeek <49596321+Kuchareeek@users.noreply.github.com>
2022-05-30 00:38:53 +0200

    Add files via upload
    Add board, drag and drop pieces, and not end attack front

---
+++ src/referee/Referee.ts
@@ -0,0 +1,25 @@
+import { PieceType } from "../components/Chessboard/Chessboard";
+
+export default class Referee {
+    isVaildMove(px:number, py:number, x:number, y:number, type:PieceType, color:string)
+    {
+
+        //wyslac do serwera i zwrocic true lub false 
+        return true
+    }
+    waitForOponent()
+    {
+        //czeka na od gdy jest to gooooo
+        //cos takiego
+        // while(true)
+        // {
+        //     if(ruchwykonany)
+        //     {
+        //         break
+        //     }
+
+        // }
+        //return ruch wroga
+
+    }
+}
